223d659a0d7d984d090453696caa83c43cc41e0d
Raj-Dev-Mahato <mahatoraj2208@gmail.com>
2025-08-14 13:50:24 +0530

    Update api.jsx

--- frontend/src/api/api.jsx
+++ frontend/src/api/api.jsx
@@ -1,7 +1,7 @@
 // src/api/api.jsx
 import axios from "axios";
 const api = axios.create({
-baseURL: import.meta.env.VITE_API_URL || "blog-app-amber-nine.vercel.app",
+baseURL: import.meta.env.VITE_API_URL || "https://blog-app-zqyl.onrender.com",
 });
 // Attach token automatically if present
 api.interceptors.request.use((config) => {
